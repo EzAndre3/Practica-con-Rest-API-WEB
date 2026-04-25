deb0b4dd27ac99da623aeec6f5743e2ba2217231
EzAndre3 <A00838689@tec.mx>
2026-04-24 22:48:12 -0600

    Push

--- controllers/login.controllers.js
+++ controllers/login.controllers.js
@@ -1,12 +1,37 @@
-import User from "../models/users.model.js"
-export const login = async (req, res)=>{
-    const {username, password} = req.body
-    const user = await User.findOne
-    ({username:username})
-    if (user.password == password){
-        res.json({login:true, msg:"Ok", user:user})
-    }else{
-        res.status(404).json({login:false,msg:"wrong",
-            user:{}})
-     }
-}
+import User from "../models/users.model.js";
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).json({
+        login: false,
+        msg: "Usuario y contraseña son obligatorios",
+        user: {}
+      });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (!user || user.password !== password) {
+      return res.status(401).json({
+        login: false,
+        msg: "Usuario o contraseña incorrectos",
+        user: {}
+      });
+    }
+
+    res.json({
+      login: true,
+      msg: "ok",
+      user
+    });
+  } catch (error) {
+    res.status(500).json({
+      login: false,
+      msg: "Error en el servidor",
+      error: error.message
+    });
+  }
+};
